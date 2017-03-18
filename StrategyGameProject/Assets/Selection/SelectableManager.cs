9f210ebfbe660926b7772d27c3f60eb9821281dd
Falsan <sam.britnell117@gmail.com>
2017-03-18 19:33:59 +0000

    Camera controls and drag selection

---
+++ StrategyGameProject/Assets/Selection/SelectableManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectableManager : MonoBehaviour {
+
+    public static SelectableManager instance = null;
+    public List<GameObject> currentSelected;
+
+    void Start ()
+    {
+        if (instance)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+	
+	void Update ()
+    {
+        for(int iter = 0; currentSelected.Count > iter; iter++)
+        {
+            Debug.Log(currentSelected[iter].name);
+        }
+	}
+
+    public void AddToCurrentSelected(GameObject toAdd)
+    {
+        currentSelected.Add(toAdd);
+    }
+
+    public void ClearCurrentSelected()
+    {
+        currentSelected.Clear();
+    }
+
+    
+}
