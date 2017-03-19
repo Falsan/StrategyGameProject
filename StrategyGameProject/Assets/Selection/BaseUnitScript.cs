4dcc250e9e5ae0732aed3d46ad533972bcc464ac
Falsan <sam.britnell117@gmail.com>
2017-03-19 14:25:17 +0000

    baseunitscript and movement orders

---
+++ StrategyGameProject/Assets/Selection/BaseUnitScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseUnitScript : MonoBehaviour {
+
+    Vector3 targetToMoveTo;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Move(Vector3 Location)
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Location);
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetToMoveTo = hit.point;
+            
+        }
+
+        //targetToMoveTo.y = gameObject.transform.position.y;
+
+        rb.MovePosition(targetToMoveTo);
+
+
+    }
+}
